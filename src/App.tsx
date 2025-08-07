8b8b3958e412c688285705fb6ede2669c7a11143
kuwa <t.kuwazaki1024@gmail.com>
2025-08-07 15:25:26 +0000

    静的ページ公開

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,124 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Button, Grid, TextField } from '@mui/material';
+import { DataGrid, type GridColDef } from '@mui/x-data-grid';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const columns: GridColDef[] = [
+  { field: 'product', headerName: '商品' },
+  { field: 'price', headerName: '金額' },
+  { field: 'quantity', headerName: '数量' },
+  { field: 'discount', headerName: '割引' },
+  { field: 'discountRate', headerName: '割引率' },
+  { field: 'unitPrice', headerName: '単価' },
+];
 
+const rows = [
+  {
+    id: 1,
+    product: '商品A',
+    price: 10000,
+    quantity: 10,
+    discount: 35,
+    discountRate: 35,
+    unitPrice: 35,
+  },
+  {
+    id: 2,
+    product: '商品B',
+    price: 10000,
+    quantity: 10,
+    discount: 42,
+    discountRate: 42,
+    unitPrice: 42,
+  },
+  {
+    id: 3,
+    product: '商品C',
+    price: 10000,
+    quantity: 10,
+    discount: 45,
+    discountRate: 45,
+    unitPrice: 45,
+  },
+  {
+    id: 4,
+    product: '商品A',
+    price: 10000,
+    quantity: 10,
+    discount: 16,
+    discountRate: 16,
+    unitPrice: 16,
+  },
+  {
+    id: 5,
+    product: '商品A',
+    price: 10000,
+    quantity: 10,
+    discount: null,
+    discountRate: null,
+    unitPrice: null,
+  },
+  {
+    id: 6,
+    product: '商品A',
+    price: 10000,
+    quantity: 10,
+    discount: 150,
+    discountRate: 150,
+    unitPrice: 150,
+  },
+  {
+    id: 7,
+    product: '商品A',
+    price: 10000,
+    quantity: 10,
+    discount: 44,
+    discountRate: 44,
+    unitPrice: 44,
+  },
+  {
+    id: 8,
+    product: '商品A',
+    price: 10000,
+    quantity: 10,
+    discount: 36,
+    discountRate: 36,
+    unitPrice: 36,
+  },
+  {
+    id: 9,
+    product: '商品A',
+    price: 10000,
+    quantity: 10,
+    discount: 65,
+    discountRate: 65,
+    unitPrice: 65,
+  },
+];
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Grid container spacing={2}>
+        <Grid size={4}>
+          <TextField id="outlined-basic0" label="商品" variant="outlined" />
+        </Grid>
+        <Grid size={4}>
+          <TextField id="outlined-basic1" label="金額" variant="outlined" />
+        </Grid>
+        <Grid size={4}>
+          <TextField id="outlined-basic2" label="数量" variant="outlined" />
+        </Grid>
+        <Grid size={4}>
+          <TextField id="outlined-basic3" label="割引" variant="outlined" />
+        </Grid>
+        <Grid size={4}>
+          <TextField id="outlined-basic4" label="割引率" variant="outlined" />
+        </Grid>
+        <Grid size={4}>
+          <Button variant="contained">Contained</Button>
+        </Grid>
+      </Grid>
+      <DataGrid rows={rows} columns={columns} checkboxSelection sx={{ border: 0 }} />
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tanka-calc/',
   plugins: [react()],
   server: {
     host: true,
